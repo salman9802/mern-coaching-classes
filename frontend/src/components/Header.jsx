362a578783bf99e3dc68606c4200fde311fae768
Salman <sallu123xx@gmail.com>
2024-12-27 10:03:21 +0530

    fix: add mobile responsiveness to header and its gap. Also added hidden to overflow-x of body

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,8 +5,8 @@ import logo from "./../assets/logo.jpg";
 const Header = () => {
   return (
     <>
-      <div className='w-screen pt-14'></div>
-      <header className='p-6 sticky top-0 z-50 bg-white w-11/12 mx-auto  flex flex-row justify-between items-center border-b border-b-gray-300'>
+      <div className='w-screen pt-5 md:pt-10 lg:pt-12'></div>
+      <header className='pt-2 p-6 sticky top-0 z-50 bg-white w-11/12 mx-auto  flex flex-row justify-between items-center border-b border-b-gray-300 md:p-8'>
         <div className='flex items-center space-x-3 text-primary-500 font-semibold'>
           <img className='w-10 h-10 md:w-11 md:h-11' src={logo} alt='Logo' />
           <span className='text-xl md:text-3xl'>Rathod Coaching Classes</span>
